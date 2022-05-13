ec23cff986a986d1fc646ae927d16f49662b447d
Jose <jlcab8@gmail.com>
2022-05-13 12:39:43 +0200

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
 import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import WorkPage from "./pages/WorkPage";
 
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutPage />
+      <Nav />
+      <Routes>
+        {/* Cuando encuentra la ruta se queda
+      y no sigue buscando más */}
+        <Route path="/" element={<AboutPage />} />
+        <Route path="/work" element={<WorkPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <NavBar>
+      <h1>
+        <Link to="/" className="logo">
+          Capture
+        </Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/about">1. About Us</Link>
+        </li>
+        <li>
+          <Link to="/work">2. Our Work</Link>
+        </li>
+        <li>
+          <Link to="/contact">3. Contact Us</Link>
+        </li>
+      </ul>
+    </NavBar>
+  );
+};
+
+const NavBar = styled.nav`
+  min-height: 10vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 10rem;
+  background-color: #282828;
+  a {
+    color: #fff;
+    text-decoration: none;
+  }
+  ul {
+    display: flex;
+    list-style: none;
+  }
+  li {
+    padding-left: 10rem;
+    position: relative;
+  }
+
+  .logo {
+    font-size: 1.5rem;
+    font-family: "Lobster", cursive;
+    font-weight: lighter;
+  }
+`;
+
+export default Nav;
